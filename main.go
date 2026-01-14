e852eb8fdbe1f7d6153af8635f80df80a73577f0
gaotao <gaotao@legoutech.cn>
2026-01-14 10:53:11 +0800

    add a new api

--- main.go
+++ main.go
@@ -6,11 +6,17 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/getprojectinfo", handlegetprojectinfo)
+	http.HandleFunc("/getprojectinfo", handlegetProjectInfo)
+	http.HandleFunc("/getprojectname", handlegetProjectName)
 	http.ListenAndServe(":8280", nil)
 }
 
-func handlegetprojectinfo(w http.ResponseWriter, r *http.Request) {
+func handlegetProjectInfo(w http.ResponseWriter, r *http.Request) {
 	var info = "a project info"
 	fmt.Fprintf(w, info)
+}
+
+func handlegetProjectName(w http.ResponseWriter, r *http.Request) {
+	var name = "a project name"
+	fmt.Fprintf(w, name)
 }
